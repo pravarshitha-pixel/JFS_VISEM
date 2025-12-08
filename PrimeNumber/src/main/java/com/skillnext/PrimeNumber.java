c3223f214b63da829e9b2d337bd635987e958699
pravarshithakolikapongu <155140802+pravarshithakolikapongu@users.noreply.github.com>
2025-12-08 10:56:43 +0530

    Initial commit: Added all Maven projects

---
+++ PrimeNumber/src/main/java/com/skillnext/PrimeNumber.java
@@ -0,0 +1,32 @@
+package com.skillnext;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Scanner;
+
+public class PrimeNumber
+ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println("Prime number");
+        else
+            System.out.println("Not a prime number");
+    }
+}
